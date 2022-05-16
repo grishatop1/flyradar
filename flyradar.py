2f2b54cd748bebdd880ae8978822c57094699044
ableunable <stefansucur0@gmail.com>
2022-05-16 15:39:16 +0200

    float to int in hover circle drawing
    also update requirements

--- flyradar.py
+++ flyradar.py
@@ -73,7 +73,7 @@ class FlightManager:
 				if m1:
 					i_panel.selectFlight(f.id)
 					remove_selection = False
-				pygame.draw.circle(win, (255,255,255), (r_x-3,r_y-3), 10,2)
+				pygame.draw.circle(win, (255,255,255), (int(r_x-3),int(r_y-3)), 10,2)
 
 			#if r_x < 0 or r_x > WIDTH or r_y < 0 or r_y > HEIGHT:
 			#	pass
@@ -228,7 +228,7 @@ class InfoPanel:
 
 		fr_x, fr_y, r_x, r_y, f_angle = f_mngr.offsetPlanes(f.longitude, f.latitude)
 
-		pygame.draw.circle(win, (255,255,255), (r_x-3,r_y-3), 10,2)
+		pygame.draw.circle(win, (255,255,255), (int(r_x-3),int(r_y-3)), 10,2)
 
 		for info, f_info in self.info:
 			data = getattr(f, f_info)
